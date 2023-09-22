673032664e85efb37ebe450ff693e514f7aa6e16
Kris Mitina <kristmitina@gmail.com>
2023-09-22 17:44:40 +0200

    Structure of the project

---
+++ RickAndMorty/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by DVKSH on 22.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
